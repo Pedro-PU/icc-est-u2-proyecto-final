5546c4b1e582d7ac2f1e9c5e1bde4ff1eeeb016b
jonnathans06 <jhonasaav@gmail.com>
2025-07-27 16:01:49 -0500

    feat: Inicialización de la clase MazePanel, colocados atributos e inicialización de constructores

--- src/views/MazePanel.java
+++ src/views/MazePanel.java
@@ -1,5 +1,23 @@
 package views;
 
-public class MazePanel {
-    
+import controllers.MazeController;
+import models.Cell;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MazePanel extends JPanel {
+    private final int rows;
+    private final int cols;
+    private final Cell[][] cells;
+    private final JButton[][] buttons;
+    private MazeController mazeController;
+
+    public MazePanel(int parametro1, int parametro2) {
+        this.rows = parametro1;
+        this.cols = parametro2;
+        this.cells = new Cell[rows][cols];
+        this.buttons = new JButton[parametro1][parametro2];
+        setLayout(new GridLayout(parametro1, parametro2));
+    }
 }
