b31f5aea2a3a1509e528d1e0afa6fdc3c5c1a05e
MHFERNANDO <fermarhug@gmail.com>
2025-07-27 19:11:47 -0500

    Modelo AlgothmResult completado

--- src/dao/daoImpl/AlgorithmResultDAOFile.java
+++ src/dao/daoImpl/AlgorithmResultDAOFile.java
@@ -1,5 +1,14 @@
 package dao.daoImpl;
 
-public class AlgorithmResultDAOFile {
+import dao.AlgorithmResultDAO;
+import models.AlgorithmResult;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgorithmResultDAOFile implements AlgorithmResultDAO {
+
     
 }
+
--- src/models/AlgorithmResult.java
+++ src/models/AlgorithmResult.java
@@ -1,5 +1,31 @@
 package models;
 
 public class AlgorithmResult {
-    
+    private final String algorithmName;
+  
+  private final int pathLength;
+  
+  private final long timeMs;
+  
+  public AlgorithmResult(String paramString, int paramInt, long paramLong) {
+    this.algorithmName = paramString;
+    this.pathLength = paramInt;
+    this.timeMs = paramLong;
+  }
+  
+  public String getAlgorithmName() {
+    return this.algorithmName;
+  }
+  
+  public int getPathLength() {
+    return this.pathLength;
+  }
+  
+  public long getTimeMs() {
+    return this.timeMs;
+  }
+  
+  public String toString() {
+    return this.algorithmName + "," + this.algorithmName + "," + this.pathLength;
+  }
 }
