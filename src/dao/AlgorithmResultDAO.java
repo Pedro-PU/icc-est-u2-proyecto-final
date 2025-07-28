62bebb0fbd3b552e727dd8d01828519b9871b349
MHFERNANDO <fermarhug@gmail.com>
2025-07-27 19:12:56 -0500

    Interface DAO creada

--- src/dao/AlgorithmResultDAO.java
+++ src/dao/AlgorithmResultDAO.java
@@ -1,6 +1,12 @@
+
 package dao;
 
+import java.util.List;
+
+import models.AlgorithmResult;
 
 public interface AlgorithmResultDAO {
-    
+    void save(AlgorithmResult resultado);
+    List<AlgorithmResult> findAll();
+    void clear();
 }
